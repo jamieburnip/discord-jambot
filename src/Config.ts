dc0dd86c0c5ebd06ba265a0638eb7dbe371aef47
Jamie Burnip <jamieburnip@gmail.com>
2019-10-02 09:36:43 +0100

    Update Config.ts

--- src/Config.ts
+++ src/Config.ts
@@ -5,8 +5,4 @@ module.exports = {
 	'token': process.env.DISCORD_TOKEN,
 	'meaning_of_life': 42,
 	'passwords_array': ['please', 'dont', 'hack', 'me'],
-	'secret_passcodes': {
-		'bank': 1234,
-		'home': 4321,
-	},
 };
